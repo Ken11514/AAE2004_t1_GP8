c8c179bdae7cf97c5b9885c4c201e0134e59eac0
Ken11514 <22075211d@connect.polyu.hk>
2022-11-08 12:57:27 +0800

    catch up

--- Task _1/A_Star_Path_Finding_Code.py
+++
@@ -1,7 +0,0 @@
-'''
-We are goging to put our code in here ~
-'''
-Gropu_Mates = ['Wilbert','Ray','Narmira','Mimi','Leon','Ken','Tomas']
-for I in Gropu_Mates :
-    print( I +' is one of the group member of Group 8')
-print('We all aggre that AAE is da best !!!!!!!!!!!!')
